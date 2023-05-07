22f76c187452cbc885b9a1e6f395f2195f1e2d74
micutio <wagner.mchl@googlemail.com>
2023-05-07 18:24:00 +0800

    ColorMyPencils now takes individual dark and light scheme

--- ubuntu/nvim/after/plugin/colors.lua
+++ ubuntu/nvim/after/plugin/colors.lua
@@ -1,25 +1,26 @@
-
-local function checkGsettings(color)
+local function pickColorFromGSettings(lightTheme, darkTheme)
     local handle = io.popen("gsettings get org.gnome.desktop.interface color-scheme")
     local result = handle:read("*a")
-   
+
     if string.find(result, "prefer-dark", 1, true) == nil then
         vim.opt.bg = 'light'
+        vim.cmd.colorscheme(lightTheme)
     else
         vim.opt.bg = 'dark'
+        vim.cmd.colorscheme(darkTheme)
     end
 
     handle:close()
 end
 
 
-function ColorMyPencils(color)
-	color = color or "adwaita"
-    checkGsettings()
-    vim.cmd.colorscheme(color)
+function ColorMyPencils(lightTheme, darkTheme)
+    lightTheme = lightTheme or "adwaita"
+    darkTheme = darkTheme or lightTheme
+    pickColorFromGSettings(lightTheme, darkTheme)
 
-	vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils()
+ColorMyPencils("rose-pine", "poimandres")
