4cdead415c588f2601f6a951034edd9d47c5c732
micutio <wagner.mchl@googlemail.com>
2026-05-09 23:55:32 +0800

    update telescope branch

--- linux/.config/nvim/lua/michael/lazy.lua
+++ linux/.config/nvim/lua/michael/lazy.lua
@@ -40,7 +40,7 @@ require("lazy").setup({
     --
     {
         'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
+        branch = 'master',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
